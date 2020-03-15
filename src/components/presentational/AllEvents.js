9dce7e7639f932551a9b35bf9b9097d51196d542
cristinaradulescu <cristina.radulescu@metrosystems.net>
2020-03-15 18:41:53 +0200

    Enhanced css

--- src/components/presentational/AllEvents.js
+++ src/components/presentational/AllEvents.js
@@ -1,10 +1,47 @@
 import React, {useEffect} from "react";
+import "./AllEvents.scss";
 
 const AllEvents = (props) => {
 
-    useEffect(() => {props.fetchAllEvents()}, [props.fetchAllEvents]);
+    useEffect(() => {
+        props.fetchAllEvents()
+    }, [props.fetchAllEvents]);
 
-    return props.events.map(event => <div key={event.id}> {event.name} </div>)
+
+    const renderHeader = () => {
+        return <div className="header">
+            <div className="title">Technical Events</div>
+        </div>
+    };
+
+    const renderEvents = () => {
+        return props.events && props.events.map(event =>
+            <div className="event-details" key={event.id}>
+
+                <div className="event-name">
+                    {event.name}
+                </div>
+
+                <div className="event-description">
+                    {event.description}
+                </div>
+
+                <button className="event-info">
+                    View Details
+                </button>
+
+                <button className="event-join">
+                    Join
+                </button>
+            </div>);
+    };
+
+
+    return <div className="main-events">
+        <div>{renderHeader()}</div>
+        <div className="events-container">{renderEvents()}</div>
+    </div>;
+
 
 };
 
